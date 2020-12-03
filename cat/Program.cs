fa50e7bba29682fa7d8773fd028680a840a57da8
Opilane TTHK <opilane@tthk.ee>
2020-12-03 12:55:26 +0200

    tere

---
+++ cat/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace FridayOOP
+{
+    class Program
+    {
+        class Cat
+        {
+            string name;
+            string colour;
+            double hunger;
+
+            public Cat(string _name, string _colour)
+            {
+                string name = _name;
+                string colour = _colour;
+                double hunger = 0;
+            }
+
+            public string Name
+            {
+                get { return name; }
+            }
+
+            public string Colour
+            {
+                get { return colour; }
+            }
+
+            public double Hunger
+            {
+                get { return hunger; }
+            }
+
+            public void Sleep()
+            {
+                hunger += 0.2;
+                Console.WriteLine("cat sleeps");
+            }
+
+            public void Screech()
+            {
+                Console.WriteLine("meow~");
+            }
+
+            public void CatData()
+            {
+                Console.WriteLine($"The cats name is {name}, colour {colour} and his hunger level: {hunger}");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Cat newCat = new Cat("Pebbles", "White n' black spots");
+            newCat.CatData();
+            while (newCat.Hunger != 1)
+            {
+                newCat.Sleep();
+            }
+
+
+            newCat.Screech();
+
+
+            newCat.CatData();
+
+
+        }
+    }
+}
---
+++ shoppinglist/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace shoppinglist
+{
+    class Program
+    {
+        public class Item
+        {
+            string name;
+            int price;
+
+            public Item(string _name, int _price)
+            {
+                name = _name;
+                price = _price;
+                Console.WriteLine($"item {name} created.");
+            }
+
+            public string Name { get { return name; } }
+
+            public int Price { get { return price; } }
+        }
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\Users\opilane\samples";
+            string fileName = @"shoppinglist.txt";
+            List<Item> shoppingItems = new List<Item>();
+            List<string> linesFromFile = File.ReadAllLines(Path.Combine(filePath, fileName)).ToList();
+
+            foreach (string line in linesFromFile)
+            {
+                List<Item> shoppingItems = new List<Item>();
+
+                List<string> linesFromFile = File.ReadLines(Path.Combine(filePath, fileName)).ToList;
+
+                foreach(string Line in ListFromFile)
+                {
+                    string[] tempArry = line.Split(new char[] {\}, StringSplitOptions.RemoveEmptyEntries);
+                    Item newitem = new Item(tempArry[0], Int32.Parse(tempArry[1])));
+                    shoppingItems.Add(newitem);
+                }
+
+                Console.WriteLine("enter shopping cart:");
+                int total = 0;
+
+                foreach(Item item in shoppingItems)
+                {
+                    Console.WriteLine($"item: {item.Name}; price: {item.Price}");
+                    total += item.Price;
+                    {
+                        Console.WriteLine($"your shopping cart total: {total}");
+                    }
+                }
+            }
+        }
+    }
+}
